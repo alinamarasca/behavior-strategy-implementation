e3865e42f87306645492c183d856c926196abf0b
alinamarasca <75176258+alinamarasca@users.noreply.github.com>
2021-05-14 12:24:04 +0200

    Update reverse-and-case.test.js

--- function-design/exercises/easy/reverse-and-case.test.js
+++ function-design/exercises/easy/reverse-and-case.test.js
@@ -8,103 +8,50 @@
  *  false: set the string to upper case
  * @returns {string} the reversed text in all lower or upper case
  */
-const stub = (text = (''), lowerCase = true) => {
-  let reversed = text.split("").reverse().join("");
-  lowerCase = confirm('OK -> lower case, CANCEL -> upper case');
-  if (lowerCase){
-    return reversed.toLowerCase();
-  }else{
-    return reversed.toUpperCase();
-  }
-  };
-//stub('wtf');
+const stub = () => {};
+
 /*
-  -get string
-  -reverse it
-  -ask if want upeer/lowercase
-  -do case
-  -give back or casified or not
+  your strategy goes here
 */
 
 for (const solution of [
-  //secretSolution,
-   stub
+  secretSolution,
+  // stub
 ]) {
   describe(
     solution.name + ': reverses a string then sets to lower or upper case',
     () => {
       describe("the function's default parameters", () => {
-// GENERal CASES
         it('second parameter defaults to true', () => {
-          expect(solution('asdf')).toEqual('fdsa'); 
+          expect(solution('asdf')).toEqual('fdsa');
         });
         it('first parameter defaults to an empty string', () => {
           expect(solution()).toEqual('');
         });
       });
       // write the tests indicated by the comments
-// separate lower and upper
       describe('when set to lower case', () => {
-        alert('say OK');
-        
-        it('when the text is an empty string', () => {
-          expect(solution('', true)).toEqual('');
+        // when the text is an empty string
+        it(_, () => {
+          expect(solution(_, _)).toEqual(_);
         });
-
-        it('when the text is all upper case', () => {
-          expect(solution('MEOW', true)).toEqual('woem');
-        });
-        
-        it('when the text is all lower case', () => {
-          expect(solution('meow', true)).toEqual('woem');
-        });
-
-        it ('when the text is mixed upper and lower case', () => {
-          expect(solution('MeOw',true)).toEqual('woem');
-        });
-
-        it ('when the text contains punctuation', () => {
-          expect(solution('meow!',true)).toEqual('!woem');
-        });
-
-        it('when the text contains numbers', () => {
-          expect(solution('MEOW13',true)).toEqual('31woem');
-        });
-
+        // when the text is all upper case
+        // when the text is all lower case
+        // when the text is mixed upper and lower case
+        // when the text contains punctuation
+        // when the text contains numbers
       });
-
       describe('when set to upper case', () => {
-        alert('say CANCEL');
-        it('when the text is an empty string', () => {
-          expect(solution('', false)).toEqual('');
-        });
-
-        it('when the text is all upper case', () => {
-          expect(solution('WOOF', false)).toEqual('FOOW');
-        });
-        
-        it('when the text is all lower case', () => {
-          expect(solution('woof',false)).toEqual('FOOW');
-        });
-
-        it ('when the text is mixed upper and lower case', () => {
-          expect(solution('wOoF',false)).toEqual('FOOW');
-        });
-
-        it ('when the text contains punctuation', () => {
-          expect(solution('...WOOF',false)).toEqual('FOOW...');
-        });
-
-        it('when the text contains numbers', () => {
-          expect(solution('woof27',false)).toEqual('72FOOW');
-        });
-
-       });
+        // when the text is an empty string
+        // when the text is all upper case
+        // when the text is all lower case
+        // when the text is mixed upper and lower case
+        // when the text contains punctuation
+        // when the text contains numbers
+      });
     }
   );
 }
-    
-   
 
 // minified solution for testing your tests
 // prettier-ignore
