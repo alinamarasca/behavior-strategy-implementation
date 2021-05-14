3aec6a2107025ba8b32444066aa1717043bd142b
alinamarasca <75176258+alinamarasca@users.noreply.github.com>
2021-05-14 12:25:52 +0200

    Update testing-paths-if-else-if-else.test.js

--- function-design/exercises/easy/testing-paths-if-else-if-else.test.js
+++ function-design/exercises/easy/testing-paths-if-else-if-else.test.js
@@ -13,7 +13,7 @@ const stub = (val1, val2) => {
   if (val1 === val2){
    return ('strictly equal');
   }
-  if (typeof val1 == val2){
+  if (typeof val1 == typeof val2){
     return ('same type')
   } else {
     return ('totally different')
@@ -24,13 +24,13 @@ const stub = (val1, val2) => {
 - get two values
 - compare
 - if val1 === val2 -> 'strictly equal'
-  if typof val == typeof val 2  -> 'same type'
+  if typeof val == typeof val 2  -> 'same type'
  else -> 'totally different'
 */
 
 for (const solution of [
-  secretSolution,
-  // stub,
+  //secretSolution,
+  stub,
 ]) {
   describe(solution.name + ': determines how similar two values are', () => {
    //check types of strictly equal
@@ -51,7 +51,7 @@ for (const solution of [
         expect(solution('hello', 'hola')).toEqual('same type');
       });
       it('numbers', () => {
-        expect(solution(2, 2.5)).toEqual('same type');
+        expect(solution(2, 3)).toEqual('same type');
       });
       it('booleans', () => {
         expect(solution(true, false)).toEqual('same type');
@@ -72,4 +72,3 @@ for (const solution of [
 // minified solution for testing your tests
 // prettier-ignore
 function secretSolution(a, b) { let c = ""; return c = a === b ? "strictly equal" : typeof a == typeof b ? "same type" : "totally different", c }
-
