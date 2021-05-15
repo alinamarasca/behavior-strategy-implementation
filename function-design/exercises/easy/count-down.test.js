53f613274cb711201bbe855248b9e5a39eddf78b
IrinaSing <irinasingh.mail@yandex.ru>
2021-05-15 22:52:48 +0200

    count-downs

--- function-design/exercises/easy/count-down.test.js
+++ function-design/exercises/easy/count-down.test.js
@@ -6,26 +6,105 @@
  *  start must be an integer that is greater than 0
  * @returns {number[]} an array of all numbers from `start` to 0
  */
-const stub = () => {};
+
+const stub = (start = 0) => {
+  return [];
+};
 
 /*
-  your strategy goes here
+1. Write if-else conditionals and return errors if the parameter does not match the description. 
+2. Write a while loop or for-loop to count down while 'start' is greater than 0. 
+3. Create a new variable with array (that supposed to be returned). All numbers that counted down push to this array.
 */
 
+//for-loop
+const timer1 = (start = 0) => {
+  if (start < 0) {
+    return 'Error: count down from a negative number';
+  } else if (isNaN(start)) {
+    return 'Error: not a number';
+  } else if (!Number.isInteger(start)) {
+    return 'Error: not an integer';
+  }
+
+  const result = [];
+   for (let i = start; i >= 0; i--) {
+    let count = i;
+    result.push(count);
+  }
+  return result;
+};
+
+//while-loop version
+const timer2 = (start = 0) => {
+  if (start < 0) {
+    return 'Error: count down from a negative number';
+  } else if (isNaN(start)) {
+    return 'Error: not a number';
+  } else if (!Number.isInteger(start)) {
+    return 'Error: not an integer';
+  }
+
+  const result = [];
+  let count = start;
+   while (count >= 0) {
+    result.push(count);
+    count = count -1;
+  }
+  return result;
+};
+
+//count up from 0 to 'start' and reverse
+const timer3 = (start = 0) => {
+  if (start < 0) {
+    return 'Error: count down from a negative number';
+  } else if (isNaN(start)) {
+    return 'Error: not a number';
+  } else if (!Number.isInteger(start)) {
+    return 'Error: not an integer';
+  }
+
+  const result = [];
+   for (let i = 0; i <= start; i++) {
+    let count = i;
+    result.push(count);
+  }
+  return result.reverse();
+};
+
+
+
 for (const solution of [
-  secretSolution,
-  // stub
+  //secretSolution,
+  //timer1, //it works
+  //timer2,  //it works
+  timer3, //it works
 ]) {
   // the main test suite for the function
   describe(solution.name + ': counts down to 0', () => {
     it('default parameter', () => {
       expect(solution()).toEqual([0]);
     });
-    it('0', () => {
+    it('if count down should be from 0', () => {
       expect(solution(0)).toEqual([0]);
     });
-    it('1', () => {
+    it('if count down should be from 1', () => {
       expect(solution(1)).toEqual([1, 0]);
+    });
+    it('if count down should be from 4', () => {
+      expect(solution(4)).toEqual([4, 3, 2, 1, 0]);
+    });
+    it('should not accept Infinity', () => {
+      expect(solution(Infinity)).toEqual('Error: not an integer');
+    });
+    it('should not accept the parameter which is not a number', () => {
+      expect(solution('strings')).toEqual('Error: not a number');
+    });
+    it('should not accept the negative number', () => {
+      expect(solution(-4)).toEqual('Error: count down from a negative number');
+    });
+    it('should not work with decimals', () => {
+      expect(solution(2.5)).toEqual('Error: not an integer');
     });
     // write at least 5 more tests ...
   });
--- function-design/exercises/easy/count-up.test.js
+++ function-design/exercises/easy/count-up.test.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * builds an array counting up from 0 to `max`
@@ -6,27 +6,94 @@
  *  max must be an integer that is greater than 0
  * @returns {number[]} an array of all numbers from 0 to `max`
  */
-const stub = () => {};
+const stub = (max = 0) => {
+  return [];
+};
 
 /*
-  your strategy goes here
+Strategy
+1. Write if-else conditionals and return errors if the parameter does not match the description. 
+2. Write a while loop or for-loop.
+3.Create a variable that becomes greater by one with each iteration until it is equal to 'max'.
+4. Create a new variable with array (that supposed to be returned). All numbers that counted up push to this array.
 */
 
+//while-loop (works)
+const timer1 = (max = 0) => {
+  if (max < 0) {
+    return "Error: number should be greater than 0";
+  } else if (typeof max !== "number") {
+    return "Error: not a number";
+  } else if (!Number.isInteger(max)) {
+    return "Error: not an integer";
+  }
+
+  const result = [];
+  let count = 0;
+  while (count <= max) {
+    result.push(count);
+    count = count + 1;
+    
+  }
+  return result;
+};
+
+//for-loop (works)
+const timer2 = (max = 0) => {
+  if (max < 0) {
+    return "Error: number should be greater than 0";
+  } else if (typeof max !== "number") {
+    return "Error: not a number";
+  } else if (!Number.isInteger(max)) {
+    return "Error: not an integer";
+  }
+
+  const result = [];
+   for (let i = 0; i <= max; i++) {
+    let count = i;
+    result.push(count);
+  }
+  return result;
+};
+
+
+
 for (const solution of [
-  secretSolution,
-  // stub
+  //secretSolution,
+  //stub,
+  //timer1, //works
+  timer2,
 ]) {
   // the main test suite for the function
-  describe(solution.name + ': counts up from 0', () => {
-    it('default parameter', () => {
+  describe(solution.name + ": counts up from 0", () => {
+    it("default parameter", () => {
       const actual = solution();
       expect(actual).toEqual([0]);
     });
-    it('0', () => {
+    it("if max = 0", () => {
       expect(solution(0)).toEqual([0]);
     });
-    it('1', () => {
+    it("if max = 1", () => {
       expect(solution(1)).toEqual([0, 1]);
+    });
+    it("if max = 4", () => {
+      expect(solution(4)).toEqual([0, 1, 2, 3, 4]);
+    });
+    it("should not accept Infinity", () => {
+      expect(solution(Infinity)).toEqual("Error: not an integer");
+    });
+    it("should not accept the parameter which is not a number", () => {
+      expect(solution("strings")).toEqual("Error: not a number");
+    });
+
+    it("should not accept the parameter which is not a number", () => {
+      expect(solution(null)).toEqual("Error: not a number");
+    });
+    it("should not accept the negative number", () => {
+      expect(solution(-4)).toEqual("Error: number should be greater than 0");
+    });
+    it("should not work with decimals", () => {
+      expect(solution(2.5)).toEqual("Error: not an integer");
     });
     // write at least 5 more tests ...
   });
