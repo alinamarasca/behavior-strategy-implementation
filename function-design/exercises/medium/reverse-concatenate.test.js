8841215042f5c9e8007741b98f26032a728d9c39
alinamarasca <75176258+alinamarasca@users.noreply.github.com>
2021-05-18 18:13:55 +0200

    Update reverse-concatenate.test.js

--- function-design/exercises/medium/reverse-concatenate.test.js
+++ function-design/exercises/medium/reverse-concatenate.test.js
@@ -6,9 +6,29 @@
  * @param {string[]} arrayOfStrings - an array of strings to concatenate, in reverse order
  * @returns {string} - the array elements joined together, in reverse order
  */
-const stub = () => {};
+'use strict'
 
-/*
+const mixArray1 = (arr = []) => {
+  let cloneArray =[...arr];
+  let newArray =[];
+  for(let index = 0; index <= cloneArray.length; index++){
+    if (typeof cloneArray[index] === 'boolean') {
+      continue;
+    }
+    let itemToPush = Number(cloneArray[index]);
+    if(Number.isNaN(itemToPush)){
+    continue;
+   } else if (typeof itemToPush === 'number'){
+   newArray.push(itemToPush)
+   }
+   }
+  return newArray
+  }
+
+console.log(mixArray1([true, '8', 9]));
+
+/*Implementation:
+
 
 */
 
